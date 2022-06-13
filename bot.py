94ae36d9809752d0edb2162a3e706eae07e3b33e
cuizzang <cuizzang1@gmail.com>
2022-06-13 22:49:20 +0900

    finish_blackjack

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import asyncio
 from typing_extensions import Self
 from winreg import DeleteValue
 
@@ -28,69 +29,124 @@ async def on_message(message):
 @bot.command(name='blackjack')
 async def black_jack(ctx):
     msg = await ctx.send("게임 시작")
+    show = ['🇦','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🔟','🇯','🇶','🇰']
     
-    await ctx.send("덱을 섞는 중...")
-    deck = []
-    for i in range(13):
-        for j in range(4):
-            deck.append(i)
-            
-    for i in range(52):
-        tpos = random.randint(0,52-i)
-        tmp = deck[tpos]
-        deck[tpos] = deck[51-i]
-        deck[51-i] = tmp
-            
-    dealer_cards = [deck[0]]
-    player_cards = [deck[1],deck[2]]
-    
-    nowCardNum = 3
-    
-    async def show_board(channel, dealer_cards, player_cards): 
-        
-        def cardCalc(cards):
-            show = ['🇦','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🔟','🇯','🇶','🇰']
-            a = 0
-            sum = 0
-            str = ""
+    async def shuffle_deck():
+        await ctx.send("덱을 섞는 중...")
+        deck = []
+        for i in range(13):
+            for j in range(4):
+                deck.append(i)
                 
-            for card in cards:
-                str += show[card] + " "
-                if card == 0:
-                    a +=1
-                if card >= 10 :
-                    card = 9
-                sum += (card+1)
-            
-            while a>0:
-                if sum <=11:
-                    sum += 10
-                    a -= 1
-                else:
-                    break
-            
-            str += "\n합계 : " + str(sum)
-            
-            return str, sum
-    
-        dstr, dsum = cardCalc(dealer_cards)
-        pstr, psum = cardCalc(player_cards)
+        for i in range(52):
+            tpos = random.randint(0,52-i)
+            tmp = deck[tpos]
+            deck[tpos] = deck[51-i]
+            deck[51-i] = tmp
         
-        embed = discord.Embed(title = "blackjack")
-        embed.add_field(name="딜러의 패", value=dstr, inline=False)
-        embed.add_field(name="플레이어의 패", value=pstr, inline=False)
+        return deck
+    
+    deck = await shuffle_deck()
+    
+    nowCardNum = 0
+    dsum = da = psum = pa = 0
+    dstr = pstr = ""
+    
+    async def hit_card(deck,nowCardNum,tsum,ta,tstr):
+        if nowCardNum == 52:
+            await shuffle_deck()
+            nowCardNum = 0
+        
+        tmp = deck[nowCardNum]
+        nowCardNum+=1
+        tstr += show[tmp] + " "
+        if tmp == 0 and ta == 0:
+            ta = 1
+        if tmp >=10:
+            tmp = 9
+        tsum += tmp+1
+        
+        if tsum<12 and ta == 1:
+            ta=2
+            tsum+=10
+        elif tsum>21 and ta == 2:
+            ta=1
+            tsum-=10
+        return nowCardNum,tsum,ta,tstr 
+        
+    nowCardNum,dsum,da,dstr = await hit_card(deck,nowCardNum,dsum,da,dstr)
+    nowCardNum,psum,pa,pstr = await hit_card(deck,nowCardNum,psum,pa,pstr)
+    nowCardNum,psum,pa,pstr = await hit_card(deck,nowCardNum,psum,pa,pstr)
+    
+    embed = discord.Embed(title = "blackjack")
+    
+    async def show_board(channel): 
+        embed.clear_fields()
+        embed.add_field(name="딜러의 패", value=(dstr+"합계 : "+str(dsum)), inline=False)
+        embed.add_field(name="플레이어의 패", value=(pstr+"합계 : "+str(psum)), inline=False)
         
         ret = await channel.send(embed = embed)
     
         return ret
     
-    board = await show_board(msg.channel,dealer_cards,player_cards)
+    board = await show_board(msg.channel)
     
     reaction_list = ['✅','🛑']
     for r in reaction_list:
         await board.add_reaction(r)
+        
     def author_check(reaction, user) : 
-        return str(reaction) in reaction_list and user == ctx.author and reaction.message.id == msg.id
+        return str(reaction) in reaction_list and user == ctx.author and reaction.message.id == board.id
     
+    while(True):
+        try:
+            reaction, _user = await bot.wait_for("reaction_add", check = author_check, timeout= 120.0)
+        except asyncio.TimeoutError:
+            await ctx.send("시간초과 되었습니다.")
+            return
+        else:
+            pass
+        
+        if str(reaction)=='✅':
+            nowCardNum,psum,pa,pstr = await hit_card(deck,nowCardNum,psum,pa,pstr)
+            embed.set_field_at(1,name="플레이어의 패", value=(pstr+"합계 : "+str(psum)), inline=False)
+            await board.edit(embed = embed)
+            await board.remove_reaction(reaction, _user)
+            if psum>21:
+                break
+        else:
+            break
+    
+    if psum>21:
+        await asyncio.sleep(1.0)
+        embed.set_field_at(1,name="플레이어의 패", value=(pstr+"Bust"), inline=False)
+        await board.edit(embed = embed)
+        await ctx.send("당신의 패배입니다.")
+        return
+    else:
+        while dsum<17:
+            await asyncio.sleep(1.0)
+            nowCardNum,dsum,da,dstr = await hit_card(deck,nowCardNum,dsum,da,dstr)
+            embed.set_field_at(0,name="딜러의 패", value=(dstr+"합계 : "+str(dsum)), inline=False)
+            await board.edit(embed = embed)
+    
+    await asyncio.sleep(1.0)
+    if dsum>21:
+        embed.set_field_at(0,name="딜러의 패", value=(dstr+"Bust"), inline=False)
+        await board.edit(embed = embed)
+        await ctx.send("당신의 승리입니다.")
+        return
+    else :
+        await asyncio.sleep(1.0)
+        
+        if psum < dsum:
+            await ctx.send("당신의 패배입니다.")
+        elif psum == dsum:
+            await ctx.send("무승부입니다.")
+        else:
+            await ctx.send("당신의 승리입니다.")
+        
+        return
+        
 
 bot.run(TOKEN)
